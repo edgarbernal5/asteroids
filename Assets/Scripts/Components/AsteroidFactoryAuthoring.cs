a725cdeb4e1ffc8a142839f35553f783cefe8880
Edgar Bernal <edgar.bernal@gmail.com>
2022-08-15 01:44:01 -0300

    Created asteroid factory with spawner.

---
+++ Assets/Scripts/Components/AsteroidFactoryAuthoring.cs
@@ -0,0 +1,38 @@
+﻿using Asteroids.Scripts;
+using System;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Components
+{
+    public class AsteroidFactoryAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+    {
+        public SceneInitialization m_initializator;
+        public GameObject[] m_asteroidsPrefabs;
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            var buffer = dstManager.AddBuffer<EntityBufferElement>(entity);
+            foreach (var asteroid in m_asteroidsPrefabs)
+            {
+                buffer.Add(new EntityBufferElement()
+                {
+                    m_entity = conversionSystem.GetPrimaryEntity(asteroid)
+                });
+            }
+
+            m_initializator.m_asteroidLibrary = entity;
+        }
+
+        public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+        {
+            referencedPrefabs.AddRange(m_asteroidsPrefabs);
+        }
+    }
+
+    public struct EntityBufferElement : IBufferElementData
+    {
+        public Entity m_entity;
+    }
+}
--- Assets/Scripts/SceneInitialization.cs
+++ Assets/Scripts/SceneInitialization.cs
@@ -1,21 +1,78 @@
+using Assets.Scripts.Components;
 using Asteroids.Scripts.Components;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Asteroids.Scripts
 {
     public class SceneInitialization : MonoBehaviour
     {
         private EntityManager entityManager;
+        public Entity m_asteroidLibrary;
+
+
+        [SerializeField] private Transform[] m_asteroidsSpawnPositions;
+
+        private Vector3[] m_spawnPositionsVectors;
+
+        private float m_currentTimer;
+        [SerializeField] private float m_asteroidSpawnFrequency = 2.0f;
 
         private void Awake()
         {
             entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            m_spawnPositionsVectors = new Vector3[m_asteroidsSpawnPositions.Length];
+            for (int i = 0; i < m_spawnPositionsVectors.Length; i++)
+            {
+                m_spawnPositionsVectors[i] = m_asteroidsSpawnPositions[i].position;
+            }
         }
 
         private void Start()
         {
             entityManager.CreateEntity(typeof(InputComponent));
+        }
+
+        private void SpawnAsteroid()
+        {
+            var buffer = entityManager.GetBuffer<EntityBufferElement>(m_asteroidLibrary);
+            var lengthOfBuffer = buffer.Length;
+            var randomAsteroidIndex = Random.Range(0, lengthOfBuffer);
+            var newAsteroid = entityManager.Instantiate(buffer[randomAsteroidIndex].m_entity);
+
+            var randomSpawnPositionIndex = Random.Range(0, m_spawnPositionsVectors.Length);
+            var spawnPosition = m_spawnPositionsVectors[randomSpawnPositionIndex];
+
+            entityManager.SetComponentData(newAsteroid, new Translation()
+            {
+                Value = spawnPosition
+            });
+
+            var randomMoveDirection = math.normalize(new float3(Random.Range(-.8f, .8f), Random.Range(-.8f, .8f), 0));
+            var randomRotation = math.normalize(new float3(Random.value, Random.value, 0));
+
+            entityManager.SetComponentData(newAsteroid, new MovementCommandsComponent()
+            {
+                m_currentMoveDirection = randomMoveDirection,
+                m_currentLinearCommand = 1,
+                m_currentAngularCommand = randomRotation
+            });
+        }
+
+        private void Update()
+        {
+            m_currentTimer += Time.deltaTime;
+
+            if (m_currentTimer > m_asteroidSpawnFrequency)
+            {
+                m_currentTimer = 0;
+                SpawnAsteroid();
+            }
+
         }
     }
 }
